79364eed07682542ad8956a638fe34c4d5003fc1
Samuel Eyo <usamueleyo@gmail.com>
2022-09-09 00:15:56 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+
 /**
- * main - main block
- * Return: 1
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
  */
 int main(void)
 {
-
 	write(2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      s);
+	      59);
+
 	return (1);
 }
