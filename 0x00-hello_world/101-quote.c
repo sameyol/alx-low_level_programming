3471cd2484b344f0ee17b5a2f334daf666989eb1
Samuel Eyo <usamueleyo@gmail.com>
2022-09-08 23:51:16 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,16 @@
+task 8
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - main block
+ * Return: 1
+ */
 int main(void)
 {
+
 	write(2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
-
+	      s);
 	return (1);
 }
