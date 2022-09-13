dd495a0c248b6a36b3ee8ffbc19e7c90719bfb11
Samuel Eyo <usamueleyo@gmail.com>
2022-09-13 14:36:35 -0700

    betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
-	 * This is the preferred style for multi-line
-	 * comments in C source code.
-	 * Please use it consistently.
-	 *
-	 * Description:  A column of asterisks on the left side,
-	 * with beginning and ending almost-blank lines.
-	 */
+ *
+ *print putchar
+ *
+ * return 0
+ */
 
 int main(void)
 {
