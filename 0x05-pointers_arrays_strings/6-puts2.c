6c83b8c6d7c522e47a24b155956d1d874657a12e
Samuel Eyo <usamueleyo@gmail.com>
2022-09-20 08:57:57 -0700

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,15 @@
 
 void puts2(char *str)
 {
-	int i = 0
-		while (str[i] != '\0')
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
 		{
-			if (i % 2 == 0)
-			{
-				_putchar(str[i]);
-			}
-			i++;
+			_putchar(str[i]);
 		}
+		i++;
+	}
 	_putchar('\n');
 }
