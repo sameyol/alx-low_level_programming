b53bf682da3130e2a69d08e2eec3d7aa3968a87a
Samuel Eyo <usamueleyo@gmail.com>
2022-09-20 05:04:31 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * check the length of a string
- * return 0
+ * _strspn - return length of string that matches values consistently
+ * @s: string to search
+ * @accept: target matches
+ * Return: number of bytes consecutively matched
  */
 
-int _strlen(char *s) 
+int _strlen(char *s)
 {
-int length = 0;
-while (s[length])
-length++;
-return (length);
+	int length = 0;
+	while (s[length])
+		length++;
+	return (length);
 }
-
